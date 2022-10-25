30c621aacbdc38f20ba757cb60b7421ea422b21b
Antonin Dallard <antonin.dallard@lirmm.fr>
2022-10-24 23:44:15 -0700

    Fix null dereference issue in ArrayInput

--- Project/Assets/McRtc/ArrayInput.cs
+++ Project/Assets/McRtc/ArrayInput.cs
@@ -13,14 +13,10 @@ namespace McRtc
             get { return m_data; }
             set
             {
-                if(value.Length == m_data.Length)
+                if (value.Length == m_data.Length)
                 {
-                    Client.SendArrayInputRequest(id, value);
-                    m_data = value;
-                }
-                else
-                {
-                    Debug.LogError($"You attempted to set data of size {value.Length} in {id} which expects size {m_data.Length}");
+                  Client.SendArrayInputRequest(id, value);
+                  m_data = value;
                 }
             }
         }
@@ -28,8 +24,8 @@ namespace McRtc
         {
             get { return m_labels; }
         }
-        private float[] m_data;
-        private string[] m_labels;
+        private float[] m_data = new float[0];
+        private string[] m_labels = new string[0];
         public void UpdateArray(string[] labels, float[] data)
         {
             if(labels.Length == 0 && m_labels.Length != data.Length)
